f5bc531984cd4740d7da8f8277b18fd11a1c34a8
Chamil Udayanga <chamiludayanga28@gmail.com>
2023-05-04 20:08:59 +0800

    changed deploy command

--- build.sh
+++ build.sh
@@ -6,13 +6,13 @@ rm -rf dist
 echo "Host Build"
 nx run host:build:production
 
-mkdir production
-cp -r dist/apps/host/* production
-cp -r dist/apps/{cart,products} production
+# mkdir production
+# cp -r dist/apps/host/* production
+# cp -r dist/apps/{cart,products} production
 
 # echo "Deploy workspace"
-nx g @nx/workspace:run-commands deploy --project=host \
-  --command="rm -rf production && mkdir production && cp -r dist/apps/host/* production && cp -r dist/apps/{cart,products} production"
+# nx g @nx/workspace:run-commands deploy --project=host \
+#   --command="rm -rf production && mkdir production && cp -r dist/apps/host/* production && cp -r dist/apps/{cart,products} production"
 
-# echo "Depoy host"
+echo "Depoy host"
 nx deploy host
